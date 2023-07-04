31b0147ec306f63dd2cf4c363e6a85981505e0e8
Retsh <retsh@ditityze.aisa>
2023-07-04 15:34:35 +0700

    web-url

--- api/app/Http/Middleware/Cors.php
+++ api/app/Http/Middleware/Cors.php
@@ -17,7 +17,7 @@ class Cors
     {
         $response = $next($request);
         $response = response($response);
-        $response->header('Access-Control-Allow-Origin', 'http://blog.example.com');
+        $response->header('Access-Control-Allow-Origin', 'http://127.0.0.1:8000');
         $response->header('Access-Control-Allow-Methods', '*');
         $response->header('Access-Control-Allow-Credentials', 'true');
         $response->header('Access-Control-Allow-Headers', 'X-CSRF-Token');
