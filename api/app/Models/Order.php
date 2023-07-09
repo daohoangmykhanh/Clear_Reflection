33b113266d88c11521ed05c43b7ebb1b71c32e8f
hungnguyen12333 <hungn12333@gmail.com>
2023-07-09 19:42:58 +0700

    Update Order.php

--- api/app/Models/Order.php
+++ api/app/Models/Order.php
@@ -11,7 +11,7 @@ class Order extends Model
     use HasFactory;
     protected $table = 'order';
     protected $primaryKey = 'order_id';
-    protected $fillable = ['order_tracking_number', 'account_id','address_id', 'coupon_id', 'total_price', 'total_quantity', 'order_status_id', 'payment_method_id', 'created_at', 'updated_at'];
+    protected $fillable = ['order_tracking_number', 'account_id', 'address_id', 'coupon_id', 'total_price', 'total_quantity', 'order_status_id', 'payment_method_id', 'created_at', 'updated_at'];
 
     public function account()
     {
